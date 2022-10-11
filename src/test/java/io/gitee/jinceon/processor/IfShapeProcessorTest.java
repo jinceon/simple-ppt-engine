bd8f03157424d23ae29ba434297f2ade543836a8
jinceon <jinceon@qq.com>
2022-10-11 17:47:52 +0800

    add unit test

--- src/test/java/io/gitee/jinceon/processor/IfShapeProcessorTest.java
+++ src/test/java/io/gitee/jinceon/processor/IfShapeProcessorTest.java
@@ -1,17 +1,9 @@
 package io.gitee.jinceon.processor;
 
-import io.gitee.jinceon.core.Chart;
 import io.gitee.jinceon.core.DataSource;
 import io.gitee.jinceon.core.SimpleEngine;
-import io.gitee.jinceon.processor.data.AgeCount;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class IfShapeProcessorTest {
 
@@ -23,5 +15,20 @@ class IfShapeProcessorTest {
         engine.setDataSource(dataSource);
         engine.process();
         engine.save("src/test/resources/test-if-shape.pptx");
+    }
+    
+    @Test
+    void supports() {
+        IfShapeProcessor processor = new IfShapeProcessor();
+        Assertions.assertEquals(true, processor.supports("#if"));
+    }
+
+    @Test
+    void parseDirective() {
+        IfShapeProcessor processor = new IfShapeProcessor();
+        DataSource dataSource = new DataSource();;
+        dataSource.setVariable("name", "延春");
+        Assertions.assertEquals(true, processor.parseDirective("#if = #name != null", dataSource));
+        Assertions.assertEquals(false, processor.parseDirective("#if = #name1 != null", dataSource));
     }
 }
--- src/test/java/io/gitee/jinceon/processor/IfSlideProcessorTest.java
+++ src/test/java/io/gitee/jinceon/processor/IfSlideProcessorTest.java
@@ -3,12 +3,11 @@ package io.gitee.jinceon.processor;
 import io.gitee.jinceon.core.DataSource;
 import io.gitee.jinceon.core.SimpleEngine;
 import io.gitee.jinceon.processor.data.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class IfSlideProcessorTest {
 
@@ -25,4 +24,25 @@ class IfSlideProcessorTest {
         engine.process();
         engine.save("src/test/resources/test-if-slide.pptx");
     }
+
+    @Test
+    void supports() {
+        IfSlideProcessor processor = new IfSlideProcessor();
+        Assertions.assertEquals(true, processor.supports("#if"));
+    }
+
+    @Test
+    void parseDirective() {
+        IfSlideProcessor processor = new IfSlideProcessor();
+        DataSource dataSource = new DataSource();
+        List users = new ArrayList<>();
+        users.add(new User("jinceon"));
+        dataSource.setVariable("users", users);
+        dataSource.setVariable("users1", null);
+        dataSource.setVariable("users2", new ArrayList<>());
+        Assertions.assertEquals(false, processor.parseDirective("#if=( #users1 != null and #users1.size() > 0)", dataSource));
+        Assertions.assertEquals(false, processor.parseDirective("#if=( #users2 != null and #users2.size() > 0)", dataSource));
+        Assertions.assertEquals(true, processor.parseDirective("#if=( #users != null and #users.size() > 0)", dataSource));
+    }
+
 }
