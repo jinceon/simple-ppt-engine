b9491836eda28fffd579871b6cef247bd742e65d
高延春 <jinceon@qq.com>
2022-10-19 16:51:29 +0800

    (test):table processor unit test

--- src/test/java/io/gitee/jinceon/processor/TableDataProcessorTest.java
+++ src/test/java/io/gitee/jinceon/processor/TableDataProcessorTest.java
@@ -4,8 +4,12 @@ import io.gitee.jinceon.core.DataSource;
 import io.gitee.jinceon.core.SimpleEngine;
 import io.gitee.jinceon.core.Table;
 import io.gitee.jinceon.processor.data.Trend;
+import org.apache.poi.xslf.usermodel.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +17,7 @@ import java.util.List;
 class TableDataProcessorTest {
 
     @Test
-    void process() {
+    void process() throws IOException {
         SimpleEngine engine = new SimpleEngine("src/test/resources/table.pptx");
         DataSource dataSource = new DataSource();
         String[] headers = new String[]{
@@ -43,6 +47,40 @@ class TableDataProcessorTest {
 
         engine.setDataSource(dataSource);
         engine.process();
-        engine.save("src/test/resources/test-table.pptx");
+        String outputfile = "src/test/resources/test-table.pptx";
+        engine.save(outputfile);
+
+        XMLSlideShow outputPpt = new XMLSlideShow(new FileInputStream(outputfile));
+        XSLFSlide slide = outputPpt.getSlides().get(0);
+        List<XSLFShape> shapes = slide.getShapes();
+        XSLFTable uiOftableA = null;
+        XSLFTable uiOftableB = null;
+        for(XSLFShape shape: shapes){
+            if(shape instanceof XSLFTable){
+                String tableId = ShapeHelper.getAlternativeText(shape);
+                if("#tableA".equals(tableId)){
+                    uiOftableA = (XSLFTable)shape;
+                }else if("#tableB".equals(tableId)){
+                    uiOftableB = (XSLFTable)shape;
+                }
+            }
+        }
+        Assertions.assertNotNull(tableA);
+        for(int row=0;row<trends.size();row++){
+            Assertions.assertEquals(uiOftableA.getCell(row+1, 0).getText(), trends.get(row).getGoods(), "tableA not equal:"+(row+1)+"-"+0);
+            Assertions.assertEquals(uiOftableA.getCell(row+1, 1).getText(), String.valueOf(trends.get(row).getTheYearBeforeLast()),"tableA not equal"+(row+1)+"-"+1);
+            Assertions.assertEquals(uiOftableA.getCell(row+1, 2).getText(), String.valueOf(trends.get(row).getLastYear()),"tableA not equal"+(row+1)+"-"+2);
+            Assertions.assertEquals(uiOftableA.getCell(row+1, 3).getText(), String.valueOf(trends.get(row).getThisYear()),"tableA not equal"+(row+1)+"-"+3);
+        }
+        Assertions.assertNotNull(tableB);
+        for(int col=0;col<trends.size();col++){
+            Assertions.assertEquals(uiOftableB.getCell(0, col+1).getText(), trends.get(col).getGoods(), "tableB not equal"+0+col);
+            Assertions.assertEquals(uiOftableB.getCell(1, col+1).getText(), String.valueOf(trends.get(col).getTheYearBeforeLast()), "tableB not equal"+1+"-"+col);
+            Assertions.assertEquals(uiOftableB.getCell(2, col+1).getText(), String.valueOf(trends.get(col).getLastYear()), "tableB not equal"+2+"-"+col);
+            Assertions.assertEquals(uiOftableB.getCell(3, col+1).getText(), String.valueOf(trends.get(col).getThisYear()), "tableB not equal"+3+"-"+col);
+        }
+        for(int row=1;row<dynamicHeader.length;row++) {
+            Assertions.assertEquals(uiOftableB.getCell(row, 0).getText(), String.valueOf(dynamicHeader[row][0]));
+        }
     }
 }
