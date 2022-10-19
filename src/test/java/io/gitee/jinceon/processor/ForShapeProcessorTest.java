565a96d17b348f4b45ab5db05fcb3669254973ab
高延春 <jinceon@qq.com>
2022-10-19 17:15:19 +0800

    (test):for-shape processor unit test

--- src/test/java/io/gitee/jinceon/processor/ForShapeProcessorTest.java
+++ src/test/java/io/gitee/jinceon/processor/ForShapeProcessorTest.java
@@ -4,8 +4,12 @@ import io.gitee.jinceon.core.DataSource;
 import io.gitee.jinceon.core.SimpleEngine;
 import io.gitee.jinceon.core.Table;
 import io.gitee.jinceon.processor.data.Trend;
+import org.apache.poi.xslf.usermodel.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +17,7 @@ import java.util.List;
 class ForShapeProcessorTest {
 
     @Test
-    void process() {
+    void process() throws IOException {
         SimpleEngine engine = new SimpleEngine("src/test/resources/for-shape.pptx");
         DataSource dataSource = new DataSource();
         String[] headers = new String[]{
@@ -38,6 +42,32 @@ class ForShapeProcessorTest {
 
         engine.setDataSource(dataSource);
         engine.process();
-        engine.save("src/test/resources/test-for-shape.pptx");
+        String outputfile = "src/test/resources/test-for-shape.pptx";
+        engine.save(outputfile);
+
+        XMLSlideShow outputPpt = new XMLSlideShow(new FileInputStream(outputfile));
+        List<XSLFSlide> slides = outputPpt.getSlides();
+        for(int page=1;page<slides.size();page++){
+            List<XSLFShape> shapes = slides.get(page).getShapes();
+            XSLFTable uiOftableA = null;
+            for(XSLFShape shape: shapes){
+                if(shape instanceof XSLFTable){
+                    uiOftableA = (XSLFTable)shape;
+                }
+            }
+            Assertions.assertNotNull(uiOftableA);
+            Assertions.assertEquals(uiOftableA.getNumberOfRows(), tableA.getRowCount());
+            Assertions.assertEquals(uiOftableA.getNumberOfColumns(), tableA.getColumnCount());
+            for(int col=0;col<trends.size();col++){
+                Assertions.assertEquals(uiOftableA.getCell(0, col+1).getText(), trends.get(col).getGoods(), "not equal"+0+col);
+                Assertions.assertEquals(uiOftableA.getCell(1, col+1).getText(), String.valueOf(trends.get(col).getTheYearBeforeLast()), "tableB not equal"+1+"-"+col);
+                Assertions.assertEquals(uiOftableA.getCell(2, col+1).getText(), String.valueOf(trends.get(col).getLastYear()), "tableB not equal"+2+"-"+col);
+                Assertions.assertEquals(uiOftableA.getCell(3, col+1).getText(), String.valueOf(trends.get(col).getThisYear()), "tableB not equal"+3+"-"+col);
+            }
+            for(int row=1;row<dynamicHeader.length;row++) {
+                Assertions.assertEquals(uiOftableA.getCell(row, 0).getText(), String.valueOf(dynamicHeader[row][0]));
+            }
+        }
+
     }
 }
