86aef92ee8561539a990a85261401e8b65754567
jinceon <jinceon@qq.com>
2024-03-10 12:03:22 +0800

    增加chart hook的示例

--- docs/processor/Chart.md
+++ docs/processor/Chart.md
@@ -9,3 +9,27 @@
 
 调用`setData(List list)`来设置数据的时候，请注意data只应包含数据。  
 考虑到java的习惯，我们也提供了`setDataWithCategories(List list, String categoryField)`来方便使用。
+
+## 定制化函数钩子
+图表是个非常常用的组件。  
+有时也会有一些定制化需求，比如说以年为范围，以月为维度，展示一个条形图。  
+那么条形图最少的时候是1月，只有1个柱。  
+条形图最多的时候是12月，有12个柱。  
+假如要求高亮当前月（以一个不同的颜色表示当前月），即最后一个柱子，在模板上是不好设置的。  
+因此增加了一个定制化函数钩子，可以在里面实现一些风格的定制化。  
+具体代码可以参见`src/test/java/io/gitee/jinceon/core/data/ChartDataProcessorHookTest.java`
+
+```java
+SimpleEngine engine = new SimpleEngine("src/test/resources/chart-hook.pptx");
+DataSource dataSource = new DataSource();
+Chart chartA = ....();
+chartA.setCustomizeFunction(xslfChart -> {
+    xslfChart.getChartSeries().get(0).getSeries(0)
+        .getDataPoint(8) // 因此示例里是9个月的数据，所有最后一个月下标是8
+        .setFillProperties(new XDDFSolidFillProperties(XDDFColor.from(0, 0,255)));
+    });
+dataSource.setVariable("chartA", chartA);
+engine.setDataSource(dataSource);
+engine.process();
+engine.save("src/test/resources/test-chart-hook.pptx");
+```
---
+++ src/test/java/io/gitee/jinceon/core/data/ChartDataProcessorHookTest.java
@@ -0,0 +1,78 @@
+package io.gitee.jinceon.core.data;
+
+import io.gitee.jinceon.core.Chart;
+import io.gitee.jinceon.core.Chart.Pair;
+import io.gitee.jinceon.core.DataSource;
+import io.gitee.jinceon.core.SimpleEngine;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.apache.poi.xddf.usermodel.XDDFColor;
+import org.apache.poi.xddf.usermodel.XDDFSolidFillProperties;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+class ChartDataProcessorHookTest {
+
+    @Test
+    void process(){
+        SimpleEngine engine = new SimpleEngine("src/test/resources/chart-hook.pptx");
+        DataSource dataSource = new DataSource();
+        Chart chartLess = chartLess();
+        Chart chartMore = chartMore();
+        dataSource.setVariable("chartLess", chartLess);
+        dataSource.setVariable("chartMore", chartMore);
+        engine.setDataSource(dataSource);
+        engine.process();
+        engine.save("src/test/resources/test-chart-hook.pptx");
+    }
+
+
+    Chart chartLess() {
+        List<Score> months = new ArrayList<>();
+        Random random = new Random();
+        for(int i=1;i<4;i++){
+            months.add(new Score(i+"月", random.nextDouble()*5));
+        }
+        Pair[] series = new Pair[]{
+                new Pair("月份", "score")
+        };
+        Chart chart = new Chart(series);
+        chart.setDataWithCategories(months, "month");
+
+        chart.setCustomizeFunction(xslfChart -> {
+            xslfChart.getChartSeries().get(0).getSeries(0).getDataPoint(2)
+                    .setFillProperties(new XDDFSolidFillProperties(XDDFColor.from(0, 255,0)));
+        });
+        return chart;
+    }
+
+    Chart chartMore() {
+        List<Score> months = new ArrayList<>();
+        Random random = new Random();
+        for(int i=1;i<10;i++){
+            months.add(new Score(i+"月", random.nextDouble()*5));
+        }
+        Pair[] series = new Pair[]{
+                new Pair("月份", "score")
+        };
+        Chart chart = new Chart(series);
+        chart.setDataWithCategories(months, "month");
+        chart.setCustomizeFunction(xslfChart -> {
+            xslfChart.getChartSeries().get(0).getSeries(0).getDataPoint(8)
+                    .setFillProperties(new XDDFSolidFillProperties(XDDFColor.from(0, 0,255)));
+        });
+        return chart;
+    }
+    @Data
+    @AllArgsConstructor
+    public static class Score {
+        private String month;
+        private Double score;
+    }
+
+}
+
