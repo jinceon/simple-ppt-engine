41b48d5925f5178db7c82a3e9f77e9367a57823a
jinceon <jinceon@qq.com>
2024-04-30 21:21:13 +0800

    增加order注释，我自己都经常忘记到底数字是不是越小越先执行

--- src/main/java/io/gitee/jinceon/core/Order.java
+++ src/main/java/io/gitee/jinceon/core/Order.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * processor处理器顺序
+ * 数字小的先执行
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Order {
